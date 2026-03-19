0affe8d888dc6421e03ba11c0663151006c1f08b
Prem Patel <prempatel6021@gmail.com>
2026-03-19 10:46:46 +0530

    bound funtionality in fun. and added some styling

--- app.js
+++ app.js
@@ -2,30 +2,9 @@ const getData = document.querySelector("#add_task");
 const addBtn = document.querySelector("#addBtn");
 const ul = document.querySelector("#Does");
 
-addBtn.addEventListener("click", () => {
+function addingTask() {
     let task = getData.value;
-    let now = new Date();
-    let timestamp = now.toLocaleString();
-
-    let new_li = document.createElement("li");
-    let deleteBtn = document.createElement("button");
-
-    new_li.innerText = timestamp + "  " + task + "  ";
-    deleteBtn.innerText = "Delete";
-
-    new_li.appendChild(deleteBtn);
-    ul.appendChild(new_li);
-
-    deleteBtn.addEventListener("click", () => {
-        new_li.remove();
-    });
-
-    getData.value = "";
-});
-
-getData.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
-        let task = getData.value;
+    if (task !== "") {
         let now = new Date();
         let timestamp = now.toLocaleString();
 
@@ -38,14 +17,29 @@ getData.addEventListener("keydown", (e) => {
             task +
             "\u00A0\u00A0\u00A0\u00A0\u00A0";
         deleteBtn.innerText = "Delete";
+        deleteBtn.classList.add("btn", "btn-outline-danger", "btn-sm");
 
-        new_li.appendChild(deleteBtn);
         ul.appendChild(new_li);
+        new_li.appendChild(deleteBtn);
 
         deleteBtn.addEventListener("click", () => {
             new_li.remove();
         });
 
         getData.value = "";
+    } else {
+        alert("don't add empty task");
+    }
+}
+
+addBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    addingTask();
+});
+
+getData.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        addingTask();
     }
 });
